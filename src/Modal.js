f101d4708d6d87c82614bb704835be26d414a335
luisluft <luberluft@gmail.com>
2021-10-26 08:17:41 -0400

    change final modal text

--- src/Modal.js
+++ src/Modal.js
@@ -12,8 +12,8 @@ const Modal = () => {
       <div className="modal-content">
         <h2>congrats</h2>
         <p>
-          You answered {((correct / questions.length) * 100).toFixed(0)}% of
-          questions correctly
+          You answered {((correct / questions.length) * 100).toFixed(0)}% (
+          {correct}/{questions.length}) of questions correctly
         </p>
         <button className="close-btn" onClick={closeModal}>
           play again
